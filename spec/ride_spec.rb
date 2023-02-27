9d633966cb55e506dc5aa40e23062e241205f7b2
Amy Spears <amspears007@gmail.com>
2023-02-27 10:27:00 -0700

    Add tests for rider log

--- spec/ride_spec.rb
+++ spec/ride_spec.rb
@@ -2,6 +2,10 @@ require_relative 'spec_helper'
 
 RSpec.describe Ride do
   let(:ride1) {Ride.new({ name: 'Carousel', min_height: 24, admission_fee: 1, excitement: :gentle })}
+  let(:visitor1)  {Visitor.new('Bruce', 54, '$10')}
+  let(:visitor2)  {Visitor.new('Tucker', 36, '$5')}
+  let(:visitor3)  {Visitor.new('Penny', 64, '$15')}
+
   it 'exists' do
     ride1 = Ride.new({ name: 'Carousel', min_height: 24, admission_fee: 1, excitement: :gentle })
     expect(ride1).to be_a(Ride)
@@ -13,5 +17,23 @@ RSpec.describe Ride do
     expect(ride1.admission_fee).to eq(1)
     expect(ride1.excitement).to eq(:gentle)
     expect(ride1.total_revenue).to eq(0)
+  end
+
+  it 'has a rider log that tracks who had ridden the ride and how many times' do
+    visitor1 = Visitor.new('Bruce', 54, '$10')
+    visitor2 = Visitor.new('Tucker', 36, '$5')
+    visitor3 = Visitor.new('Penny', 64, '$15')
+    visitor1.add_preference(:gentle)
+    visitor2.add_preference(:gentle)
+    ride1.board_rider(visitor1)
+    ride1.board_rider(visitor2)
+    ride1.board_rider(visitor1)
+
+    expect(ride1.rider_log).to eq({
+      visitor1 => 2,
+      visitor2 =>1
+      })
+    expect(visitor1.spending_money).to eq(8)
+    expect(visitor2.spending_money).to eq(4)
   end
 end
