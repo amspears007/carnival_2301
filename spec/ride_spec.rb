3341250857d523d1e58fbd7d2c4047b7c2a957de
Amy Spears <amspears007@gmail.com>
2023-02-27 09:56:20 -0700

    Add test for attributes in Ride

--- spec/ride_spec.rb
+++ spec/ride_spec.rb
@@ -6,4 +6,12 @@ RSpec.describe Ride do
     ride1 = Ride.new({ name: 'Carousel', min_height: 24, admission_fee: 1, excitement: :gentle })
     expect(ride1).to be_a(Ride)
   end
+
+  it 'has a name, min_height, admission_fee, excitement and starts with no revenue' do
+    expect(ride1.name).to eq('Carousel')
+    expect(ride1.min_height).to eq(24)
+    expect(ride1.admission_fee).to eq(1)
+    expect(ride1.excitement).to eq(:gentle)
+    expect(ride1.total_revenue).to eq(0)
+  end
 end
