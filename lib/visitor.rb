3d85ef53800f5d83bbb5f4a52f50fe8c1cce0c8d
Amy Spears <amspears007@gmail.com>
2023-02-27 09:24:05 -0700

    Add preferences test and method in Visitor class

--- lib/visitor.rb
+++ lib/visitor.rb
@@ -10,5 +10,8 @@ class Visitor
     @spending_money = spending_money.delete('$').to_i
     @preferences = []
   end
-  
+
+  def add_preference(preference)
+    @preferences << preference
+  end
 end
